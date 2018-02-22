6fc5fe7a7de3fcde32825293d94b11e9026b9c57
keenanjacob <kjacob1@mail.csuchico.edu>
2018-02-22 13:12:02 -0800

    The program now will add items and will tell you when there is no room in cart

--- main.cpp
+++ main.cpp
@@ -4,22 +4,37 @@ using namespace std;
 
 int main()
 {
+  string item;
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
 
     do
     {
-
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
+    if (input== 'a' || input== 'A')
+    {
+    if (numItems<5)
+    {
+    cout<<"What is the item?"<<endl;
+    cin>>item;
+    list[numItems]=item;
+    numItems++;
+    }
+    else
+    {
+    cout<< "You'll need a bigger list!";
+    }
+    
+    }
+
   } 
   while(!(input=='Q'|| input=='q'));
   
-
   return 0;
 }
