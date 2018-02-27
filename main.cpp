14462f7ed0dce304f6c4fefd37bb2e98a35985d4
keenanjacob <kjacob1@mail.csuchico.edu>
2018-02-26 17:38:20 -0800

    This now prints out a list when you type q

--- main.cpp
+++ main.cpp
@@ -5,38 +5,42 @@ using namespace std;
 
 int main()
 {
-  string item;
-  string list[5]; //array of 5 strings
-  int numItems = 0;
-  char input;
+        string item;
+        string list[5]; //array of 5 strings
+        int numItems = 0;
+        char input;
 
-    do
-    {
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
-    if (input== 'a' || input== 'A')
-    {
-    cout<< "What is the item?"<<endl;
-        if (numItems<5)
+        do
         {
-        cin>>item;
-        list[numItems]=item;
-        numItems++;
-        }
-        else
-        {
-        cin>>item;
-        cout<< "You'll need a bigger list!"<<endl;
-        }
-    
-    }
+                cout<<"\n==GROCERY LIST MANAGER==";
+                cout<<"\nEnter your choice: ";
+                cout<<"\n (A)dd an item";
+                cout<<"\n (Q)uit";
+                cout<<"\nYour choice (A/Q): ";
+                cin>>input;
+                if (input== 'a' || input== 'A')
+                {
+                        cout<< "What is the item?"<<endl;
+                        if (numItems<5)
+                        {
+                                cin>>item;
+                                list[numItems]=item;
+                                numItems++;
+                        }
+                        else
+                        {
+                                cin>>item;
+                                cout<< "You'll need a bigger list!"<<endl;
+                        }
 
-  } 
-  while(!(input=='Q'|| input=='q'));
-  
-  return 0;
+                }
+        } 
+        while(!(input=='Q'|| input=='q'));
+
+        for (int i=0; i<5; i++)
+        {
+                cout<<i+1<< " " <<list[i]<<endl;
+        }
+
+        return 0;
 }
